28e2729bb036c5068a955bfaaf4a7407ae92ef7c
Charissa Clark <charissaclark21@gmail.com>
2019-08-28 17:04:01 -0600

    Added more variables

--- 2D_Platformer/Assets/Scripts/Assignments/Variables.cs
+++ 2D_Platformer/Assets/Scripts/Assignments/Variables.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class Variables : MonoBehaviour
 {
     string name = "Charissa";
+    int age  = 18;
+    float height = 5.4f;
+    bool married = false; 
+
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(name);
+        Debug.Log("First Name: " + name);
+        Debug.Log("Age: " + age);
+        Debug.Log("Height: " + height);
+        Debug.Log("Married: " + married);
     }
 
     
