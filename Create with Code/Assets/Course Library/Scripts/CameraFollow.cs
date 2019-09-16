8ba1cc247aafddf431bf3086e9d8d3bf513805c2
Charissa Clark <charissaclark21@gmail.com>
2019-09-16 17:07:16 -0600

    Added Camera Follow

--- Create with Code/Assets/Course Library/Scripts/CameraFollow.cs
+++ Create with Code/Assets/Course Library/Scripts/CameraFollow.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class CameraFollow : MonoBehaviour
 {
-    public float speed = 20;
+    public GameObject player;
+    private Vector3 offset = new Vector3 (0, 7, -8);
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +16,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.position = player.transform.position + offset;
     }
 }
