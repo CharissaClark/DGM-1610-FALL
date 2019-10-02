c9b35b705d6c4bc38b05d548aba0d86a6121edb0
Charissa Clark <charissaclark21@gmail.com>
2019-10-02 17:06:30 -0600

    Added Projectile and Animal Destroy and Move Forward

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -7,11 +7,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject projectilePrefab;
 
     // Update is called once per frame
     void Update()
@@ -26,5 +22,12 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+
     }
 }
